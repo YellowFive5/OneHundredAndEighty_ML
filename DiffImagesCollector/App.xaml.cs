9352ed25a364a350887c8cf04a966a6fa83e75f2
YellowFive <yellowfive555@yahoo.com>
2021-04-17 12:28:10 +0300

    DiffImagesCollector project add

---
+++ DiffImagesCollector/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿#region Usings
+
+using System.Windows;
+
+#endregion
+
+namespace DiffImagesCollector
+{
+    public partial class App
+    {
+    }
+}
---
+++ DiffImagesCollector/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿#region Usings
+
+#endregion
+
+namespace DiffImagesCollector
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
