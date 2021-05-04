0d4d59d88b417b50779739dc74d2cd1074afaafe
YellowFive <yellowfive555@yahoo.com>
2021-05-04 13:27:22 +0300

    model save/load

--- BinaryLearning/Program.cs
+++ BinaryLearning/Program.cs
@@ -17,21 +17,33 @@ namespace BinaryLearning
         {
             var projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../"));
             var dataSetDir = Path.GetFullPath(Path.Combine(projectDir, "../../DataSet/Сlear_bull_25_x4/Images"));
-            var workspace = Path.Combine(projectDir, "workspace");
-            // var assets = Path.Combine(projectDir, "assets");
-
-            MLContext mlContext = new MLContext();
 
             var imagesData = LoadImagesFromDirectory(dataSetDir);
 
+            MLContext mlContext = new MLContext();
             IDataView imgData = mlContext.Data.LoadFromEnumerable(imagesData);
-            IDataView shuffle = mlContext.Data.ShuffleRows(imgData);
+
+            // var model = TrainModel(mlContext, imgData, dataSetDir);
+            // mlContext.Model.Save(model, imgData.Schema, "binaryModel.zip");
+
+            DataViewSchema modelSchema;
+            var model = mlContext.Model.Load(Path.Combine(projectDir, "../../DataSet/Сlear_bull_25_x4/binaryModel.zip"), out modelSchema);
+
+            // ClassifyImg(mlContext, imgData, model);
+
+            Console.WriteLine("Hello ML!");
+            Console.ReadKey();
+        }
+
+        private static ITransformer TrainModel(MLContext mlContext, IDataView dataView, string dataSetDir)
+        {
+            IDataView shuffledData = mlContext.Data.ShuffleRows(dataView);
             var preprocessingPipeline = mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Label",
                                                                                       outputColumnName: "LabelKey")
                                                  .Append(mlContext.Transforms.LoadRawImageBytes(outputColumnName: "Img",
                                                                                                 imageFolder: dataSetDir,
                                                                                                 inputColumnName: "ImgPath")); // > InputData.cs
-            IDataView preProcData = preprocessingPipeline.Fit(shuffle).Transform(shuffle);
+            IDataView preProcData = preprocessingPipeline.Fit(shuffledData).Transform(shuffledData);
             DataOperationsCatalog.TrainTestData trainSplit = mlContext.Data.TrainTestSplit(data: preProcData, testFraction: 0.25);
             DataOperationsCatalog.TrainTestData validationTestSplit = mlContext.Data.TrainTestSplit(trainSplit.TestSet);
 
@@ -54,12 +66,7 @@ namespace BinaryLearning
             var trainingPipeline = mlContext.MulticlassClassification.Trainers.ImageClassification(classifierOptions)
                                             .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
-            ITransformer trainedModel = trainingPipeline.Fit(trainSet);
-
-            ClassifyOneImg(mlContext, testSet, trainedModel);
-
-            Console.WriteLine("Hello ML!");
-            Console.ReadKey();
+            return trainingPipeline.Fit(trainSet);
         }
 
         private static IEnumerable<ImgData> LoadImagesFromDirectory(string folder)
@@ -80,20 +87,19 @@ namespace BinaryLearning
             }
         }
 
+        private static void ClassifyImg(MLContext myContext, IDataView data, ITransformer trainedModel)
+        {
+            PredictionEngine<InputData, Output> predEngine = myContext.Model.CreatePredictionEngine<InputData, Output>(trainedModel);
+            InputData image = myContext.Data.CreateEnumerable<InputData>(data, reuseRowObject: true).ElementAt(new Random().Next(1, 515));
+            Output prediction = predEngine.Predict(image);
+            Console.WriteLine("Prediction for single image");
+            OutputPred(prediction);
+        }
+
         private static void OutputPred(Output pred)
         {
             string imgName = Path.GetFileName(pred.ImgPath);
             Console.WriteLine($"Image: {imgName} | Actual Label: {pred.Label} | Predicted Label: {pred.PredictedLabel}");
-        }
-
-        private static void ClassifyOneImg(MLContext myContext, IDataView data, ITransformer trainedModel)
-        {
-            PredictionEngine<InputData, Output> predEngine = myContext.Model.CreatePredictionEngine<InputData, Output>(trainedModel);
-            InputData image = myContext.Data.CreateEnumerable<InputData>(data, reuseRowObject: true).First();
-            Output prediction = predEngine.Predict(image);
-            //print predicted value
-            Console.WriteLine("Prediction for single image");
-            OutputPred(prediction);
         }
     }
 }
