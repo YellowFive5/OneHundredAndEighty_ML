5f236afe07722ae86bf406985c5104d886ab0b7b
YellowFive <yellowfive555@yahoo.com>
2023-10-22 16:55:12 +0300

    Proj add, scripts add

---
+++ BlenderSyntheticDataset/25_2.py
@@ -0,0 +1,79 @@
+import bpy
+import uuid
+
+def render(value):
+    output_file = "L:/Synthetic/25/" + value + "_" + str(uuid.uuid4()) + ".png"
+    bpy.context.scene.render.resolution_x = 1920
+    bpy.context.scene.render.resolution_y = 1080
+    bpy.context.scene.render.image_settings.file_format = "PNG"
+    bpy.ops.render.render(write_still=True)
+    bpy.data.images["Render Result"].save_render(output_file)
+
+def moveXYClock(clock, times = 1): #12 2 4 6 8 10
+    for x in range(times):
+        if clock == 12:
+            bpy.ops.transform.translate(value=(0, 0.024, 0))
+        elif clock == 2:
+            bpy.ops.transform.translate(value=(0.0208, 0.012, 0))
+        elif clock == 4:
+            bpy.ops.transform.translate(value=(0.0208, -0.012, 0))
+        elif clock == 6:
+            bpy.ops.transform.translate(value=(0, -0.024, 0))
+        elif clock == 8:
+            bpy.ops.transform.translate(value=(-0.0208, -0.012, 0))
+        elif clock == 10:
+            bpy.ops.transform.translate(value=(-0.0208, 0.012, 0))
+
+def center(): # center
+    bpy.ops.view3d.snap_selected_to_cursor(use_offset=True)
+
+def stick(deep, times = 1):
+    for x in range(times):
+        bpy.ops.transform.translate(value=(0, 0, deep))
+
+def rotateY(rads, times = 1):
+    for x in range(times):
+        bpy.ops.transform.rotate(value=rads, orient_axis='Y')
+        
+def rotateX(rads, times = 1):
+    for x in range(times):
+        bpy.ops.transform.rotate(value=rads, orient_axis='X')
+        
+def rotateZ(rads, times = 1):
+    for x in range(times):
+        bpy.ops.transform.rotate(value=rads, orient_type='LOCAL', orient_axis='Z')
+        
+# _________________________________________________________________________
+
+renders = 0;
+movesXY = [6,6,2,4,2,10,10,12,4,4,4,12,10,10,2,4,4,2,10,10,12,4,4]
+yxRoatatesAng = -0.05236
+yRoatatesTimes = 20
+xRoatatesTimes = 15
+zRoatatesAng = 0.3927
+zRoatatesTimes = 3
+# 79200
+
+moveXYClock(6, 5)
+
+for x in movesXY:
+    bpy.data.objects["Dart_2"].rotation_euler = (0,0,0)
+    bpy.ops.transform.rotate(value=0.5236, orient_axis='Y')
+    bpy.ops.transform.rotate(value=0.7854, orient_axis='X')
+    for zr in range(zRoatatesTimes):
+        rotateZ(zRoatatesAng)
+        for yr in range(yRoatatesTimes):
+            for xr in range(xRoatatesTimes):
+                render("bull")
+                renders = renders + 1
+                print(str(renders) + "/20700")
+                rotateX(yxRoatatesAng)
+            rotateX(yxRoatatesAng * xRoatatesTimes * -1)
+            rotateY(yxRoatatesAng)
+        rotateY(yxRoatatesAng * yRoatatesTimes * -1)
+    rotateZ(zRoatatesAng * zRoatatesTimes * -1)
+    moveXYClock(x)
+
+bpy.ops.transform.rotate(value=-0.7854, orient_axis='X')
+bpy.ops.transform.rotate(value=-0.5236, orient_axis='Y')
+bpy.data.objects["Dart_2"].rotation_euler = (0,0,0)
---
+++ BlenderSyntheticDataset/Base.py
@@ -0,0 +1,100 @@
+import bpy
+import uuid
+
+#bpy.ops.view3d.snap_selected_to_cursor(use_offset=True) # center
+#bpy.ops.transform.translate(value=(0, 0.024, 0)) # step top
+#bpy.ops.transform.translate(value=(0, -0.024, 0)) # step down
+#bpy.ops.transform.translate(value=(0.024, 0, 0)) # step right
+#bpy.ops.transform.translate(value=(-0.024, 0, 0)) # step left
+#bpy.ops.transform.translate(value=(0.0208, 0.012, 0)) # step top/r
+#bpy.ops.transform.translate(value=(-0.0208, 0.012, 0)) # step top/l
+#bpy.ops.transform.translate(value=(0.0208, -0.012, 0)) # step down/r
+#bpy.ops.transform.translate(value=(-0.0208, -0.012, 0)) # step down/l
+
+def render(value):
+    #output_file = "C:/Users/YellowFive/Desktop/Synthetic/" + value + "_" + str(uuid.uuid4()) + ".png"
+    output_file = "Z:/Synthetic/test/" + value + "_" + str(uuid.uuid4()) + ".png"
+    bpy.context.scene.render.resolution_x = 1920
+    bpy.context.scene.render.resolution_y = 1080
+    bpy.context.scene.render.image_settings.file_format = "PNG"
+    bpy.ops.render.render(write_still=True)
+    bpy.data.images["Render Result"].save_render(output_file)
+
+def moveXYClock(clock, times = 1): #12 2 4 6 8 10
+    for x in range(times):
+        if clock == 12:
+            bpy.ops.transform.translate(value=(0, 0.024, 0))
+        elif clock == 2:
+            bpy.ops.transform.translate(value=(0.0208, 0.012, 0))
+        elif clock == 4:
+            bpy.ops.transform.translate(value=(0.0208, -0.012, 0))
+        elif clock == 6:
+            bpy.ops.transform.translate(value=(0, -0.024, 0))
+        elif clock == 8:
+            bpy.ops.transform.translate(value=(-0.0208, -0.012, 0))
+        elif clock == 10:
+            bpy.ops.transform.translate(value=(-0.0208, 0.012, 0))
+
+def center(): # center
+    bpy.ops.view3d.snap_selected_to_cursor(use_offset=True)
+
+def stick(deep, times = 1):
+    for x in range(times):
+        bpy.ops.transform.translate(value=(0, 0, deep))
+
+def rotateY(rads, times = 1):
+    for x in range(times):
+        bpy.ops.transform.rotate(value=rads, orient_axis='Y')
+        
+def rotateX(rads, times = 1):
+    for x in range(times):
+        bpy.ops.transform.rotate(value=rads, orient_axis='X')
+        
+def rotateZ(rads, times = 1):
+    for x in range(times):
+        bpy.ops.transform.rotate(value=rads, orient_type='LOCAL', orient_axis='Z')
+        
+# _________________________________________________________________________
+
+renders = 0;
+movesXY = [12,12,4,4,6,6,8,8,10,10,12,12,2,6,6,4,2,12]
+yxRoatatesAng = -0.05236
+yRoatatesTimes = 20
+xRoatatesTimes = 15
+zRoatatesAng = 0.3927
+zRoatatesTimes = 3
+
+# 1.5708 - 90
+# 1.0472 - 60
+# 0.7854 - 45
+# 0.5236 - 30
+# 0.392699 - 22.5
+# 0.05236 - 3
+
+
+for x in movesXY:
+    bpy.data.objects["Dart_2"].rotation_euler = (0,0,0)
+    bpy.ops.transform.rotate(value=0.5236, orient_axis='Y')
+    bpy.ops.transform.rotate(value=0.7854, orient_axis='X')
+    for zr in range(zRoatatesTimes):
+        rotateZ(zRoatatesAng)
+        for yr in range(yRoatatesTimes):
+            for xr in range(xRoatatesTimes):
+                render("bull")
+                renders = renders + 1
+                print(str(renders))
+                rotateX(yxRoatatesAng)
+            rotateX(yxRoatatesAng * xRoatatesTimes * -1)
+            rotateY(yxRoatatesAng)
+        rotateY(yxRoatatesAng * yRoatatesTimes * -1)
+    rotateZ(zRoatatesAng * zRoatatesTimes * -1)
+    moveXYClock(x)
+
+bpy.ops.transform.rotate(value=-0.7854, orient_axis='X')
+bpy.ops.transform.rotate(value=-0.5236, orient_axis='Y')
+moveXYClock(8)
+bpy.data.objects["Dart_2"].rotation_euler = (0,0,0)
+#bpy.data.objects["Dart_2"].location = (-4.97982, -10.8899, -7.26813)
+
+
+
