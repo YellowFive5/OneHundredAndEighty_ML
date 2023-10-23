b5c071cbbe2d408046c01437a6837259ef043446
YellowFive <yellowfive555@yahoo.com>
2023-10-23 10:22:36 +0300

    RenderImagesConverter proj init

---
+++ RenderImagesConverter/Program.cs
@@ -0,0 +1,16 @@
+﻿#region Usings
+
+using System;
+
+#endregion
+
+namespace RenderImagesConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
