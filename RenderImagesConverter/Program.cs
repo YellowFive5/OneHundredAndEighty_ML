c5cdbff9d604cbf3f4f62e29cf5195f68e487489
YellowFive <yellowfive555@yahoo.com>
2023-10-24 11:46:38 +0300

    RenderImagesConverter complete

--- RenderImagesConverter/Program.cs
+++ RenderImagesConverter/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Emgu.CV;
 using Emgu.CV.Structure;
@@ -18,15 +19,20 @@ namespace RenderImagesConverter
             var sourceFolder = @"Z:\SyntheticData\";
             var sourceFolderBull = @$"{sourceFolder}Renders\Bull\";
             var sourceFolder25 = $@"{sourceFolder}Renders\25\";
-            var destFolderBull = $@"{sourceFolder}Dataset\Bull\";
-            var destFolder25 = $@"{sourceFolder}Dataset\25\";
+            var destFolderBull = $@"{sourceFolder}Dataset\Projection\Bull\";
+            var destFolder25 = $@"{sourceFolder}Dataset\Projection\25\";
+            // var destFolder25 = @"D:\25\";
 
             var renderClearBackground = new Image<Bgr, byte>($"{sourceFolder}RenderClearBackground.png");
 
             var dir1 = new DirectoryInfo(sourceFolderBull);
             var dir2 = new DirectoryInfo(sourceFolder25);
+            var dirOut = new DirectoryInfo(destFolder25);
+            var readyFiles = dirOut.GetFiles("*.jpeg");
+
             var files = dir2.GetFiles("*.png");
             var filesCount = files.Length;
+
             var processedCounter = 0;
             Parallel.ForEach(files,
                              new ParallelOptions
@@ -35,6 +41,12 @@ namespace RenderImagesConverter
                              },
                              f =>
                              {
+                                 if (readyFiles.Any(rf => rf.Name.Replace(".jpeg", "") == f.Name.Replace(".png", "")))
+                                 {
+                                     Console.WriteLine($"{processedCounter++}/{filesCount}");
+                                     return;
+                                 }
+
                                  var nextThrowImage = new Image<Bgr, byte>(f.FullName);
                                  var warpedDiff = ip.ConvertImage(renderClearBackground, nextThrowImage);
                                  var warpOnProjection = Drawer.DrawProjection(warpedDiff[2].Convert<Bgr, byte>());
